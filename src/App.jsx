7cfc2c8a7fd623e0139bc63c93def12963415fdd
cpzz18 <robbincfa@gmail.com>
2026-06-12 08:44:40 +0700

    feat: add AboutApp section to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { useGSAP } from '@gsap/react'
 
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
-import FeaturesSection from './components/FeaturesSection'
+import AboutSection from './components/AboutSection'
 import ProcessSection from './components/ProcessSection'
 import ParticlesBackground from './components/ParticlesBackground'
 import Footer from './components/Footer'
+import AboutAppSection from './components/AboutSection'
 
 gsap.registerPlugin(ScrollTrigger, useGSAP)
 
@@ -18,7 +19,7 @@ export default function App() {
       <Header />
       <main>
         <HeroSection />
-        <FeaturesSection />
+        <AboutAppSection />
         <ProcessSection />
       </main>
       <Footer />
